a03811df093dfd7ea2cc5eea52b4e26b2c7b7e9f
munetsugu tamura <tsweblabo.2016@gmail.com>
2021-02-13 17:18:06 +0900

    add mainMenu
    メニュー画面のUI作成：mainMenu.pug
    ルーティングの指定：mainMenu.js app.js
    LOG OUTボタン押下時にLOG IN画面に遷移する機能追加

--- myapp/app.js
+++ myapp/app.js
@@ -5,6 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+var mainMenuRouter = require('./routes/mainMenu');
+
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -20,6 +22,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/mainMenu', mainMenuRouter);
+
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
